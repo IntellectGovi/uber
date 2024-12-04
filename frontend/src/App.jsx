41856960ac226aae6eca779fd58d2a0b1b3fe072
G O V I N D   U P A D H Y A Y <govind.upadhayay19@gmail.com>
2024-12-04 23:48:39 +0530

    Login&Signup Form✅

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { Route , Routes} from 'react-router-dom'
+import Home from './pages/Home'
+import UserLogin from './pages/UserLogin'
+import CaptainLogin from './pages/CaptainLogin'
+import UserRegister from './pages/UserRegister'
+import CaptainRegister from './pages/CaptainRegister'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/userLogin' element={<UserLogin/>}/>
+        <Route path='/captainLogin' element={<CaptainLogin/>}/>
+        <Route path='/userSignup' element={<UserRegister/>}/>
+        <Route path='/captainSignup' element={<CaptainRegister/>}/>
+      </Routes>
+    </div>
   )
 }
 
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import tailwindcss from "tailwindcss";
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: '0.0.0.0', // This allows other devices on the network to connect
+  },
   plugins: [react()],
 })
